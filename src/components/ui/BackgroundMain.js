1a4de4486150fed4e25d56e1d4770110c12f5eb4
Abdelrahman Emara <abdelrahmanemara155@gmail.com>
2026-03-04 04:04:58 +0200

    fix: prevent image from shrinking on mobile screens

--- src/components/ui/BackgroundMain.js
+++ src/components/ui/BackgroundMain.js
@@ -1,11 +1,11 @@
-import BackgroundV2 from "./BackgroundAuth";
+import BackgroundAuth from "./BackgroundAuth";
 
 export default function BackgroundMain({ children, className = "" }) {
   return (
     <div className="relative h-auto bg-primary-500">
-      <BackgroundV2 className="absolute inset-0 pointer-events-none z-1" >
+      <BackgroundAuth className="absolute inset-0 pointer-events-none z-1" >
         
-      </BackgroundV2>
+      </BackgroundAuth>
       <div className={`px-xl3 relative top-0 left-0 ${className || ""} z-10`}>
         {children}
       </div>
